9d3c618d5413a26a24f0b150eb001fe80c9f5815
tim <591669730@qq.com>
2019-08-20 17:58:06 +0800

    加载默认配置

--- Core/start.php
+++ Core/start.php
@@ -6,8 +6,12 @@
  * Time: 11:39
  */
 require_once "./Core/Core/Start.php";
+#加载框架默认配置
+$frame_config = require_once './Core/Config/config.php';
 #定义变量
-define('EXT','.php');
+define('EXT', '.php');
+
+define('FRAME_CONFIG', $frame_config);
 //define('MODULE_NAME') or define('MODULE_NAME','Home');
 
 
